c4c2cc191c6497ff6dc9440cb358732eab8cb5b2
Anna Leonenko <mskozlovskaya@gmail.com>
2016-12-02 21:41:19 +0100

    lesson 9

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 (function () {
 'use strict';
 
-angular.module('NameCalculator', [])
+angular.module('DependencyInj', [])
 
-.controller('NameCalculatorController', function ($scope) {
-  $scope.name = "";
-  $scope.totalValue = 0;
+.controller('DIController', DIController)//['$scope', '$filter', DIController]);
+function DIController ($scope, $filter, $injector) {
+  $scope.name = "Anna";
 
-  $scope.displayNumeric = function () {
-    var totalValue = calculateNumericForString($scope.name);
-
-    $scope.totalValue = totalValue;
+  $scope.upper = function () {
+    var upCase = $filter('uppercase');
+    $scope.name = upCase($scope.name);
   };
 
-function calculateNumericForString (string) {
-  var totalStringValue = 0;
-  for (var i = 0; i < string.length; i++) {
-    totalStringValue += string.charCodeAt(i);
-  }
-  return totalStringValue
+  console.log ($injector.annotate(DIController));
 }
 
-});
+function AnnotateMe(name, job, blah) {
+  return "string";
+}
 
+console.log(DIController.toString())
 
 })();
