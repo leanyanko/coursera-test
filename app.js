7341ec031b9a45fe5dc85f86e450cd0e41d74b8e
Anna Leonenko <mskozlovskaya@gmail.com>
2016-12-02 17:00:14 +0100

    Name Calculator

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 (function () {
-  'use strict';
+'use strict';
 
-  angular.module('testApp', [])
+angular.module('NameCalculator', [])
 
-  .controller('Controller', function ($scope) {
-      $scope.name = "Scope";
-      $scope.sayHello = function () {
-        return "Hello there";
-      }
-  });
+.controller('NameCalculatorController', function ($scope) {
+  $scope.name = "";
+  $scope.totalValue = 0;
+
+  $scope.displayNumeric = function () {
+    var totalValue = calculateNumericForString($scope.name);
+
+    $scope.totalValue = totalValue;
+  };
+
+function calculateNumericForString (string) {
+  var totalStringValue = 0;
+  for (var i = 0; i < string.length; i++) {
+    totalStringValue += string.charCodeAt(i);
+  }
+  return totalStringValue
+}
+
+});
+
+
 })();
