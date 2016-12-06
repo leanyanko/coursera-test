4aea74485086999ee507bbf87d6671abedf7d279
Anna Leonenko <mskozlovskaya@gmail.com>
2016-12-06 22:08:26 +0100

    lecture 14

--- app.js
+++ app.js
@@ -6,11 +6,35 @@ angular.module('CounterApp', [])
 
 CounterController.$inject = ['$scope'];
 function CounterController($scope) {
+  $scope.onceCounter = 0;
+  $scope.counter = 0;
+  $scope.name = 'name';
 
   $scope.showNumberOfWatchers = function () {
     console.log('#watchers', $scope.$$watchersCount);
   };
 
+  $scope.countOnce = function() {
+    $scope.onceCounter = 1;
+  };
+
+  $scope.$watch(function () {
+    console.log('caught!');
+  })
+  // $scope.$watch('onceCounter', function (newValue, oldValue) {
+  //   console.log('Old value', oldValue);
+  //   console.log('New value', newValue);
+  // });
+  //
+  // $scope.$watch('counter', function (newValue, oldValue) {
+  //   console.log('Old value', oldValue);
+  //   console.log('New value', newValue);
+  // });
+
+  $scope.upCounter = function () {
+    $scope.counter++;
+  };
 }
+
 
 })();
