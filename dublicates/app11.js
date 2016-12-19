e648622267492cfb9a6b70c37dd29774ecab2da1
Anna Leonenko <mskozlovskaya@gmail.com>
2016-12-19 17:58:59 +0100

    lesson 23

---
+++ dublicates/app11.js
@@ -0,0 +1,27 @@
+(function () {
+'use strict';
+
+angular.module('DependencyInj', [])
+.controller('DIController', DIController);
+DIController.$inject = ['$scope', '$filter'];
+//.controller('DIController', ['$scope', '$filter', DIController]);
+function DIController ($scope, $filter, $injector) {
+  $scope.name = "Anna";
+
+  $scope.upper = function () {
+    var upCase = $filter('uppercase');
+    $scope.name = upCase($scope.name);
+  };
+}
+
+})();
+
+// !function(){"use strict";function n(n,e,r){
+//   n.name="Anna",n.upper=function(){var r=e("uppercase");
+//   n.name=r(n.name)}}angular.module("DependencyInj",[]).
+//   controller("DIController",["$scope","$filter",n])}();
+
+// !function(){"use strict";function n(n,e,r){
+//   n.name="Anna",n.upper=function(){var r=e("uppercase");
+//   n.name=r(n.name)}}angular.module("DependencyInj",[])
+//   .controller("DIController",["$scope","$filter",n])}();
