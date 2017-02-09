8d426862d77f93b6777c69df3476797d9f9c8ed3
Anna Leonenko <mskozlovskaya@gmail.com>
2017-02-09 11:59:02 -0500

    39

--- src/routes.js
+++ src/routes.js
@@ -21,6 +21,19 @@
           return ShoppingListService.getItems();
         }]
       }
+    })
+    .state('itemDetail', {
+      url: '/item-detail/{itemId}',
+      templateUrl: 'src/shoppinglist/templates/item-detail.template.html',
+      controller: 'ItemDetailController as itemDetail',
+      resolve: {
+        item: ['$stateParams', 'ShoppingListService',
+                function ($stateParams, ShoppingListService) {
+                  return ShoppingListService.getItems().then(function (items) {
+                    return items[$stateParams.itemId];
+                  })
+                }]
+      }
     });
   }
 
