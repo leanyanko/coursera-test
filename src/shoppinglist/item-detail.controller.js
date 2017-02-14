890d9c868279c579769ee9f3ce50f92794d4d3e4
Anna Leonenko <mskozlovskaya@gmail.com>
2017-02-13 19:45:36 -0500

    40

--- src/shoppinglist/item-detail.controller.js
+++ src/shoppinglist/item-detail.controller.js
@@ -5,9 +5,10 @@ angular.module('ShoppingList')
 .controller('ItemDetailController', ItemDetailController);
 
 // 'item' is injected through state's resolve
-ItemDetailController.$inject = ['item']
-function ItemDetailController(item) {
+ItemDetailController.$inject = ['$stateParams','items']
+function ItemDetailController($stateParams, items) {
   var itemDetail = this;
+  var item = items [$stateParams.itemId];
   itemDetail.name = item.name;
   itemDetail.quantity = item.quantity;
   itemDetail.description = item.description;
